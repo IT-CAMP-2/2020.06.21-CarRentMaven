f4e0d3c1ec6bf1931b263cff3d702dd7246738ae
Mateusz <bmatteo@interia.pl>
2020-06-21 17:18:38 +0200

    KOmentarz

--- src/main/java/pl/camp/it/db/VehicleRepository.java
+++ src/main/java/pl/camp/it/db/VehicleRepository.java
@@ -13,26 +13,7 @@ public class VehicleRepository {
     private static final VehicleRepository vehicleRepository = new VehicleRepository();
 
     private VehicleRepository() {
-
-
-
-
-
-        /*Car c1 = new Car(1, "BMW", "3", "jkasydg");
-
-        Car c2 = new Car(2, "Toyota", "Corolla", "yuuyruru");
-
-        Car c3 = new Car(3, "Audi", "A5", "qry6535qwy");
-
-        this.vehicles.add(c1);
-        this.vehicles.add(c2);
-        this.vehicles.add(c3);
-
-        this.vehicles.add(new Bus(4, "Solaris", "152",
-                "hjsgdfa", 40, 6));
-
-        this.vehicles.add(new Bus(5, "Solaris", "A100",
-                "t6is6adsdy", 45, 6));*/
+        Persistance.loadData(vehicles);
     }
 
     public List<Vehicle> getVehicles() {
